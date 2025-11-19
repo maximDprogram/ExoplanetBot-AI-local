d55f57f5f693152c935e44b5841f2bc976446a49
Maxim Danilov <105491819+maximDprogram@users.noreply.github.com>
2025-11-19 14:40:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ dp = Dispatcher()
 
 # Инициализация OpenAI
 openai = OpenAI(
-    base_url='http://localhost:11434/api',
+    base_url='http://localhost:11434/v1/',
     api_key=OPENAI_API_KEY,
 )
 
